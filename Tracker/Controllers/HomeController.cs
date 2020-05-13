6e9912e36df6e8496eb662506def1c7aea5456eb
Jason Robbins <jrobbin4@my.westga.edu>
2020-05-13 11:58:20 -0400

    Built the update function:
    Bugs can now be updated. Engineers can be assigned and reassigned and descriptions can be updated.

--- Tracker/Controllers/HomeController.cs
+++ Tracker/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using System.Reflection;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Http;
 
 namespace Tracker.Controllers
 {
@@ -47,6 +48,9 @@ namespace Tracker.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly BugDAL bugDal;
+        private Bug bug;
+
+        public Object Session { get; private set; }
 
         /// <summary>
         /// Constructor for the HomeController.
@@ -71,8 +75,8 @@ namespace Tracker.Controllers
                 TempData["3"] = 0;
 
                 //List<Bug> closedBugList = this.bugDal.GetOpenBugs(User.Identity.Name);
-                this.bugDal.GetOpenBugs(User.Identity.Name).Clear();
-                return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                this.bugDal.GetAllBugsFromDatabase(User.Identity.Name).Clear();
+                return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
             }
             else
             {
@@ -98,6 +102,7 @@ namespace Tracker.Controllers
                 BugName = name,
                 Description = description,
                 Author = User.Identity.Name,
+                AssignedEngineer = "Not Assigned",
                 DateOpened = DateTime.Now,
                 DateClosed = DateTime.Now
             };
@@ -107,19 +112,19 @@ namespace Tracker.Controllers
             if (success)
             {
                 TempData["Referrer"] = "SaveRegister";
-                this.bugDal.GetOpenBugs(User.Identity.Name).Clear();
-                return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                this.bugDal.GetAllBugsFromDatabase(User.Identity.Name).Clear();
+                return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
             }
             else
             {
                 TempData["Referrer"] = "NO";
-                this.bugDal.GetOpenBugs(User.Identity.Name).Clear();
-                return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                this.bugDal.GetAllBugsFromDatabase(User.Identity.Name).Clear();
+                return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
             }
         }
 
         /// <summary>
-        /// Iterates through a List</Bug> and returns the appropriate bug.
+        /// Performs the search. Iterates through a List</Bug> and returns the appropriate bug.
         /// </summary>
         [HttpGet]
         public virtual ActionResult Index(string id)
@@ -134,29 +139,31 @@ namespace Tracker.Controllers
 
             if (intResultTryParse == true && User.Identity.IsAuthenticated)
             {
-                foreach (var bug in this.bugDal.GetOpenBugs(User.Identity.Name))
+                foreach (var bug in this.bugDal.GetAllBugsFromDatabase(User.Identity.Name))
                 {
                     if (bug.BugId == idInt)
                     {
                         success = true;
+                        
                         TempData["id"] = bug.BugId;
+                        TempData["search"] = bug.BugId;
                         TempData["1"] = 1;
-                        return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                        return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
                     }
                 }
             }
             else if (intResultTryParse == false && User.Identity.IsAuthenticated)
             {
                 TempData["2"] = 2;
-                this.bugDal.GetOpenBugs(User.Identity.Name).Clear();
-                return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                this.bugDal.GetAllBugsFromDatabase(User.Identity.Name).Clear();
+                return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
             }
 
             if (success == false && User.Identity.IsAuthenticated)
             {
                 TempData["3"] = 3;
-                this.bugDal.GetOpenBugs(User.Identity.Name).Clear();
-                return View(this.bugDal.GetOpenBugs(User.Identity.Name));
+                this.bugDal.GetAllBugsFromDatabase(User.Identity.Name).Clear();
+                return View(this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
             }
             else if (success == false && !User.Identity.IsAuthenticated)
             {
@@ -167,21 +174,79 @@ namespace Tracker.Controllers
         }
 
         /// <summary>
-        /// Calls the update.
+        /// Reads the data from the UpdateCloseAndDelete form and calls the corresponding method.
         /// </summary>
-        /// [HttpPost]
-        public IActionResult Update(Bug bug)
+        [HttpPost]
+        public IActionResult UpdateCloseAndDelete(string id, string engineer, string description, string update, string close, string delete)
         {
-
             TempData["id"] = -1;
             TempData["1"] = 0;
             TempData["2"] = 0;
             TempData["3"] = 0;
 
-            TempData["id"] = bug.BugId;
-            TempData["1"] = 1;
+            Bug oldBug = new Bug();
+            bool intResultTryParse = int.TryParse(id, out int idInt);
+            if (intResultTryParse == true && User.Identity.IsAuthenticated)
+            {
+                foreach (Bug bug in this.bugDal.GetAllBugsFromDatabase(User.Identity.Name))
+                {
+                    if (bug.BugId == idInt)
+                    {
+                        oldBug = bug;
+                        oldBug.BugId = bug.BugId;
+                        oldBug.BugName = bug.BugName;
+                        oldBug.SoftwareName = bug.SoftwareName;
+                        oldBug.Description = bug.Description;
+                    }
+                }
+            }
 
-            return View("Index", this.bugDal.GetOpenBugs(User.Identity.Name));
+            string updatedDescription;
+            if (string.IsNullOrEmpty(description))
+            {
+                updatedDescription = oldBug.Description;
+            }
+            else
+            {
+                updatedDescription = oldBug.Description + " " + "<< UPDATE >> Submitted on " + DateTime.Now + ": " + description;
+            }
+
+            Bug newBug = new Bug
+            {
+                BugId = idInt,
+                AssignedEngineer = engineer,
+                Description = updatedDescription,
+                Author = User.Identity.Name,
+            };
+
+            if (!string.IsNullOrEmpty(update))
+            {
+                bool success = this.bugDal.UpdateBug(newBug, oldBug, User.Identity.Name);
+
+                if (success)
+                {
+                    TempData["id"] = idInt;
+                    TempData["1"] = 1;
+                    return View("Index", this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+
+            else if (!string.IsNullOrEmpty(close))
+            {
+                this.bugDal.CloseBug(idInt, User.Identity.Name);
+            }
+            else if (!string.IsNullOrEmpty(delete))
+            {
+                this.bugDal.DeleteBug(idInt, User.Identity.Name);
+            }
+
+            TempData["id"] = idInt;
+            TempData["1"] = 1;
+            return View("Index", this.bugDal.GetAllBugsFromDatabase(User.Identity.Name));
         }
 
         /// <summary>
--- Tracker/DAL/BugDAL.cs
+++ Tracker/DAL/BugDAL.cs
@@ -20,12 +20,14 @@ namespace Tracker.DAL
                     "software_name, " +
                     "description, " +
                     "author," +
+                    "assigned_engineer," + 
                     "open_date) " +
                "VALUES(" +
                     "@bugName, " +
                     "@softwareName, " +
                     "@description, " +
                     "@author, " +
+                    "@assignedEngineer, " +
                     "current_timestamp)";
 
             using NpgsqlConnection connection = DBConnection.GetConnection();
@@ -35,6 +37,7 @@ namespace Tracker.DAL
             command.Parameters.AddWithValue("@softwareName", bug.SoftwareName);
             command.Parameters.AddWithValue("@description", bug.Description);
             command.Parameters.AddWithValue("@author", bug.Author);
+            command.Parameters.AddWithValue("@assignedEngineer", bug.AssignedEngineer);
 
             connection.Open();
             int count = command.ExecuteNonQuery();
@@ -50,15 +53,15 @@ namespace Tracker.DAL
         }
 
         /// <summary>        /// Returns the open bugs from the Bug table in the Tracker Database.        /// </summary>
-        public List<Bug> GetOpenBugs(string author)
+        public List<Bug> GetAllBugsFromDatabase(string author)
         {
             List<Bug> openBugList = new List<Bug>();
 
-            string selectStatement = "SELECT id, bug_name, software_name, assigned_engineer, open_date " +
+            string selectStatement = "SELECT id, bug_name, software_name, description, assigned_engineer, open_date, close_date " +
                                      "FROM bug " +
-                                     "WHERE author = @author AND close_date IS NULL " +
+                                     "WHERE author = @author " +
                                      "GROUP BY open_date, software_name, assigned_engineer, id " +
-                                     "ORDER BY open_date DESC";
+                                     "ORDER BY software_name, open_date DESC";
 
             using NpgsqlConnection connection = DBConnection.GetConnection();
             NpgsqlCommand command = new NpgsqlCommand(selectStatement, connection);
@@ -69,28 +72,41 @@ namespace Tracker.DAL
                 int id = Convert.ToInt32(reader["id"]);
                 string name = reader["bug_name"].ToString();
                 string software = reader["software_name"].ToString();
-                string engineer = "";
+                string description = reader["description"].ToString();
+                string engineer;
 
-                if (reader["assigned_engineer"] == null)
+                if (reader["assigned_engineer"] == null || reader["assigned_engineer"] == DBNull.Value)
                 {
                     engineer = "";
-
                 }
                 else
                 {
                     engineer = reader["assigned_engineer"].ToString();
+
                 }
 
                 DateTime opened = (DateTime)reader["open_date"];
+                DateTime closed;
+
+                if (reader["close_date"] == null || reader["close_date"] == DBNull.Value)
+                {
+                    closed = new DateTime();
+                }
+                else
+                {
+                    closed = (DateTime)reader["close_date"];
+                }
 
                 Bug bug = new Bug
                 {
                     BugId = id,
                     BugName = name,
                     SoftwareName = software,
+                    Description = description,
                     Author = author,
                     AssignedEngineer = engineer,
                     DateOpened = opened,
+                    DateClosed = closed,
                 };
 
                 openBugList.Add(bug);
@@ -99,78 +115,51 @@ namespace Tracker.DAL
             return openBugList;
         }
 
-
-        /// <summary>        /// Returns the closed bugs from the Bug table in the Tracker Database.        /// </summary>
-        public List<Bug> GetClosedBugs(string author)
+        /// <summary>        /// Updates and returns the selected bug.        /// </summary>
+        public bool UpdateBug(Bug newBug, Bug oldBug, string author)
         {
-            List<Bug> closedBugList = new List<Bug>();
-
-            string selectStatement = "SELECT id, bug_name, software_name, assigned_engineer, close_date " +
-                                     "FROM bug " +
-                                     "WHERE author = @author AND close_date IS NOT NULL " +
-                                     "GROUP BY close_date, software_name, assigned_engineer, id " +
-                                     "ORDER BY close_date DESC";
+            string updateStatement =                    "UPDATE bug SET " +                        "description = @newDescription, " +                        "assigned_engineer = @newEngineer " +
+                    "WHERE " +                        "id = @id AND " +                        "author = @author AND " +                        "description = @oldDescription AND " +                        "assigned_engineer = @oldEngineer ";
 
             using NpgsqlConnection connection = DBConnection.GetConnection();
-            NpgsqlCommand command = new NpgsqlCommand(selectStatement, connection);
-            command.Parameters.AddWithValue("@author", author);
-            connection.Open();            using NpgsqlDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            NpgsqlCommand command = new NpgsqlCommand(updateStatement, connection);
+
+            command.Parameters.AddWithValue("@oldDescription", oldBug.Description);
+            command.Parameters.AddWithValue("@oldEngineer", oldBug.AssignedEngineer);
+
+            command.Parameters.AddWithValue("@newDescription", newBug.Description);
+            command.Parameters.AddWithValue("@newEngineer", newBug.AssignedEngineer);
+
+            command.Parameters.AddWithValue("@id", newBug.BugId);
+            command.Parameters.AddWithValue("@author", newBug.Author);
+
+            connection.Open();
+            int count = command.ExecuteNonQuery();
+
+            if (count > 0)
             {
-                int id = Convert.ToInt32(reader["id"]);
-                string name = reader["bug_name"].ToString();
-                string software = reader["software_name"].ToString();
-                string engineer = "";
-
-                if (reader["assigned_engineer"] == null)
-                {
-                    engineer = "";
-
-                }
-                else
-                {
-                    engineer = reader["assigned_engineer"].ToString();
-                }
-
-                DateTime opened = (DateTime)reader["open_date"];
-                DateTime closed = (DateTime)reader["close_date"];
-
-                Bug bug = new Bug
-                {
-                    BugId = id,
-                    BugName = name,
-                    SoftwareName = software,
-                    Author = author,
-                    AssignedEngineer = engineer,
-                    DateOpened = opened,
-                    DateClosed = closed
-                };
-
-                closedBugList.Add(bug);
+                return true;
             }
-
-            return closedBugList;
-        }
-
-        /// <summary>        /// Updates and returns the selected bug.        /// </summary>
-        public List<Bug> UpdateBug(string author)
-        {
-            List<Bug> closedBugList = new List<Bug>();
-            return closedBugList;
+            else
+            {
+                return false;
+            };
         }
 
         /// <summary>        /// Closes and returns the selected bug.        /// </summary>
-        public List<Bug> CloseBug(string author)
+        public bool CloseBug(int id, string author)
         {
             List<Bug> closedBugList = new List<Bug>();
-            return closedBugList;
+
+            return true;
         }
 
         /// <summary>        /// Deletes the selected bug.        /// </summary>
-        public List<Bug> DeleteBug(string author)
+        public bool DeleteBug(int id, string author)
         {
             List<Bug> closedBugList = new List<Bug>();
-            return closedBugList;
+
+            return true;
         }
     }
 }
